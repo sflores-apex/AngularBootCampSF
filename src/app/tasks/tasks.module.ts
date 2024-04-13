7c389528c412a13fc83513e210d951f13093e5c7
Sarahi Flores <sflores@apexsystems.com>
2024-04-12 19:00:52 -0700

    Add tasks structure folder

---
+++ src/app/tasks/tasks.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TasksComponent } from './tasks.component';
+
+@NgModule({
+  declarations: [
+    TasksComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TasksComponent
+  ]
+})
+export class TasksModule { }
