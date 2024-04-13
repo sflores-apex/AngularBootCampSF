5260008c3ac068b04d587f9aaee16e3c6c34c4d8
Sarahi Flores <sflores@apexsystems.com>
2024-04-12 23:28:12 -0700

    Add pipe exercise

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,13 +1,18 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CapitalizePipe } from './task-seven/capitalize.pipe';
+import { TaskSevenComponent } from './task-seven/task-seven.component';
 import { TasksComponent } from './tasks.component';
 
 @NgModule({
   declarations: [
-    TasksComponent
+    TasksComponent,
+    TaskSevenComponent,
+    CapitalizePipe
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CurrencyPipe
   ],
   exports: [
     TasksComponent
