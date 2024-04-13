d3a56db3b23a0500372298b461d880ceba0fb5c0
Sarahi Flores <sflores@apexsystems.com>
2024-04-13 10:38:42 -0700

    Add task nine estructure

---
+++ src/app/tasks/task-nine/task-nine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-nine',
+  templateUrl: './task-nine.component.html',
+  styleUrl: './task-nine.component.scss'
+})
+export class TaskNineComponent {
+
+}
