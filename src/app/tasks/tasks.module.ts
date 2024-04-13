c2febaa34fd7fa164b8fb329633a510054f2b71c
Sarahi Flores <sflores@apexsystems.com>
2024-04-12 19:54:28 -0700

    Add task two component files

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TasksComponent } from './tasks.component';
+import { TaskTwoComponent } from './task-two/task-two.component';
 
 @NgModule({
   declarations: [
-    TasksComponent
+    TasksComponent,
+    TaskTwoComponent
   ],
   imports: [
     CommonModule
