bb06e92c229869b0a4626a86229d0d722b35cbf0
Sarahi Flores <sflores@apexsystems.com>
2024-04-12 23:58:07 -0700

    Add angular material exercise

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,13 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { FormsModule } from '@angular/forms';
+import { DialogNameComponent } from './task-eight/dialog-name/dialog-name.component';
+import { TaskEightComponent } from './task-eight/task-eight.component';
 import { TasksComponent } from './tasks.component';
 
 @NgModule({
   declarations: [
-    TasksComponent
+    TasksComponent,
+    TaskEightComponent,
+    DialogNameComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   exports: [
     TasksComponent
