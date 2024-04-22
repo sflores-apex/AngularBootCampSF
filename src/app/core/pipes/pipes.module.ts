912f53aed874a0bae071b67d6329d05107889b67
Sarahi Flores <sflores@apexsystems.com>
2024-04-21 18:39:08 -0700

    Add rounds up pipe to prices

--- src/app/core/pipes/pipes.module.ts
+++ src/app/core/pipes/pipes.module.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ListFilteringPipe } from './list-filtering.pipe';
+import { RoundUpPipe } from './round-up.pipe';
 
 
 
 @NgModule({
   declarations: [
-    ListFilteringPipe
+    ListFilteringPipe,
+    RoundUpPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ListFilteringPipe
+    ListFilteringPipe,
+    RoundUpPipe
   ]
 })
 export class PipesModule { }
