9de6aefb1e6b33dd6bc91dabbb370b0ae327fd43
Sarahi Flores <sflores@apexsystems.com>
2024-04-23 21:22:50 -0700

    Refactor to reactive programming

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable, catchError, of, switchMap } from 'rxjs';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, catchError, of } from 'rxjs';
 import { Item } from '../../models/item';
 import { ProductsService } from '../../services/products.service';
 
@@ -11,18 +11,20 @@ import { ProductsService } from '../../services/products.service';
 })
 export class ProductDetailComponent {
 
-  protected productById$: Observable<Item | undefined> = this.route.paramMap.pipe(
-    switchMap(params => {
-      const idProduct = String(params.get('id'));
-      return this.productsService.productById$(idProduct);
-    }),
-    catchError((error: Error): Observable<Item | undefined> => {
+  protected productById$: Observable<Item> = this.productsService.productDetail$.pipe(
+    catchError((error: Error): Observable<Item> => {
       console.error('There was an error retrieving the product: %0', error);
-      return of(undefined);
+      return of({} as Item);
     })
   );
 
   constructor(private readonly route: ActivatedRoute, private readonly productsService: ProductsService) { }
+
+  public ngAfterViewInit(): void {
+    this.route.params.subscribe((params: Params): void => {
+      this.productsService.selectProduct(params['id']);
+    });
+  }
 
   protected getDiscountPrice(price: number, discount: number): number {
     return price - (price * (discount / 100));
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,19 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, Subject, combineLatest, map } from 'rxjs';
 import { Item } from '../models/item';
+
+type Item_ID = Item['id'];
 
 @Injectable()
 export class ProductsService {
 
   private urlData = '/assets/products-data.json';
+  private productSelectedSubject$$: Subject<Item_ID> = new Subject<Item_ID>();
+  private productSelected$: Observable<Item_ID> = this.productSelectedSubject$$.asObservable();
 
   constructor(private readonly httpClient: HttpClient) { }
 
   products$: Observable<Item[]> = this.httpClient.get<Item[]>(this.urlData);
 
-  productById$ = (id: string): Observable<Item | undefined> => this.httpClient.get<Item[]>(this.urlData).pipe(
-    map((products: Item[]) => products.find(p => p.id === id))
+  productDetail$: Observable<Item> = combineLatest({
+    products: this.products$,
+    productSelected: this.productSelected$,
+  }).pipe(
+    map(({ products, productSelected }: { products: Item[]; productSelected: Item_ID; })
+      : Item => products.find(p => p.id === productSelected) as Item)
   );
+
+  selectProduct(id: Item_ID): void {
+    this.productSelectedSubject$$.next(id);
+  }
 
 }
